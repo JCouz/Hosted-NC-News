4c97e93d5a6d7064b109cbe10fdb471261d65579
JCouz <couzensj@live.co.uk>
2022-04-04 16:06:09 +0100

    fix test cases

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -38,6 +38,152 @@ describe("GET /api/topics", () => {
   });
 });
 
+describe("GET /api/articles", () => {
+  test("Status 200 - Returns an array of articles including comment_count excluding body", () => {
+    return request(app)
+      .get("/api/articles")
+      .expect(200)
+      .then(({ body: articles }) => {
+        articles.forEach((article) => {
+          expect(article).toEqual(
+            expect.objectContaining({
+              article_id: expect.any(Number),
+              title: expect.any(String),
+              topic: expect.any(String),
+              author: expect.any(String),
+              created_at: expect.any(String),
+              votes: expect.any(Number),
+              comment_count: expect.any(Number),
+            })
+          );
+        });
+      });
+  });
+  test("Status 200 - Returns an array of articles using default sort and ordering", () => {
+    return request(app)
+      .get("/api/articles")
+      .expect(200)
+      .then(({ body: articles }) => {
+        expect(articles).toBeSortedBy("created_at", { descending: true });
+      });
+  });
+  test("Status 200 - should return an array of articles objects sorted by created_at DESC", () => {
+    return request(app)
+      .get("/api/articles?sort_by=created_at")
+      .expect(200)
+      .then(({ body: articles }) => {
+        expect(articles).toBeSortedBy("created_at", { descending: true });
+      });
+  });
+  test("Status 200 - should return an array of articles whose topic is cats", () => {
+    return request(app)
+      .get("/api/articles?topic=cats")
+      .expect(200)
+      .then(({ body: articles }) => {
+        articles.forEach((article) => {
+          expect(article.topic).toEqual("cats");
+        });
+      });
+  });
+  test("Status 200 - should return an array of articles objects sorted by created_at ", () => {
+    return request(app)
+      .get("/api/articles?sort_by=created_at&order=ASC")
+      .expect(200)
+      .then(({ body: articles }) => {
+        expect(articles).toBeSortedBy("created_at", { descending: false });
+      });
+  });
+  test("Status 400 - invalid sort_by query ", () => {
+    return request(app).get("/api/articles?sort_by=bananas").expect(400);
+  });
+  test("Status 400 - invalid order query ", () => {
+    return request(app).get("/api/articles?order=bananas").expect(400);
+  });
+  test("Status 404 - invalid topic query ", () => {
+    return request(app).get("/api/articles?topic=bananas").expect(404);
+  });
+  test("Status 200 - valid topic query but no articles match so reponds with empty array", () => {
+    return request(app).get("/api/articles?topic=paper").expect(200);
+  });
+});
+
+describe("POST /api/articles/:article_id/comments", () => {
+  test("status:201 - should return an object containing the added comment", () => {
+    return request(app)
+      .post("/api/articles/1/comments")
+      .send({ username: "butter_bridge", body: "Hello World!" })
+      .expect(201)
+      .then((response) => {
+        expect(response.body).toEqual(
+          expect.objectContaining({
+            comment_id: expect.any(Number),
+            author: "butter_bridge",
+            body: "Hello World!",
+            created_at: expect.any(String),
+            votes: 0,
+          })
+        );
+      });
+  });
+  test("status:201 - should return an object containing the added comment ignoring unnecessary properties", () => {
+    return request(app)
+      .post("/api/articles/1/comments")
+      .send({
+        username: "butter_bridge",
+        body: "Hello World!",
+        prop1: "ABC",
+        prop2: 456,
+      })
+      .expect(201)
+      .then((response) => {
+        expect(response.body).toEqual(
+          expect.objectContaining({
+            comment_id: expect.any(Number),
+            author: "butter_bridge",
+            body: "Hello World!",
+            created_at: expect.any(String),
+            votes: 0,
+          })
+        );
+      });
+  });
+  test("Status 400 - invalid id", () => {
+    return request(app)
+      .post("/api/articles/no-an-id/comments")
+      .send({
+        username: "butter_bridge",
+        body: "Hello World!",
+      })
+      .expect(400);
+  });
+  test("Status 404 - Non existent id", () => {
+    return request(app)
+      .post("/api/articles/9999/comments")
+      .send({
+        username: "butter_bridge",
+        body: "Hello World!",
+      })
+      .expect(404);
+  });
+  test("Status 400 - missing required field", () => {
+    return request(app)
+      .post("/api/articles/1/comments")
+      .send({
+        username: "butter_bridge",
+      })
+      .expect(400);
+  });
+  test("Status 404 - username does not exist", () => {
+    return request(app)
+      .post("/api/articles/1/comments")
+      .send({
+        username: "mr_nobody",
+        body: "Hello World!",
+      })
+      .expect(404);
+  });
+});
+
 describe("GET /api/articles/:article_id", () => {
   test("status:200 - should return an article object containing 7 keys", () => {
     return request(app)
@@ -152,35 +298,6 @@ describe("GET /api/users", () => {
   });
 });
 
-describe("GET /api/articles", () => {
-  test("status 200 - should return an array of articles objects sorted by created_at DESC", () => {
-    return request(app)
-      .get("/api/articles?sort_by=created_at")
-      .expect(200)
-      .then(({ body: articles }) => {
-        expect(articles).toBeSortedBy("created_at", { descending: true });
-      });
-  });
-  test("status 200 - should return an array of articles whose topic is cats", () => {
-    return request(app)
-      .get("/api/articles?topic=cats")
-      .expect(200)
-      .then(({ body: articles }) => {
-        articles.forEach((article) => {
-          expect(article.topic).toEqual("cats");
-        });
-      });
-  });
-  test("status 200 - should return an array of articles objects sorted by created_at ", () => {
-    return request(app)
-      .get("/api/articles?sort_by=created_at&order=ASC")
-      .expect(200)
-      .then(({ body: articles }) => {
-        expect(articles).toBeSortedBy("created_at", { descending: false });
-      });
-  });
-});
-
 describe("GET /api/articles/:article_id", () => {
   test("status:200 - should return an article object with a comment count of 11", () => {
     return request(app)
@@ -204,7 +321,7 @@ describe("GET /api/articles/:article_id", () => {
 });
 
 describe("GET /api/articles/:article_id/comments", () => {
-  test("status:200 - should return an array of comments for the specified article_id", () => {
+  test("Status:200 - should return an array of comments for the specified article_id", () => {
     return request(app)
       .get("/api/articles/1/comments")
       .expect(200)
@@ -223,28 +340,16 @@ describe("GET /api/articles/:article_id/comments", () => {
         });
       });
   });
-});
-
-describe("POST /api/articles/:article_id/comments", () => {
-  test("status:200 - should return an object containing the added comment", () => {
-    return request(app)
-      .post("/api/articles/1/comments")
-      .send({ username: "butter_bridge", body: "Hello World!" })
-      .expect(200)
-      .then((response) => {
-        expect(response.body).toEqual(
-          expect.objectContaining({
-            comment_id: expect.any(Number),
-            author: "butter_bridge",
-            body: "Hello World!",
-            created_at: expect.any(String),
-            votes: 0,
-          })
-        );
-      });
+  test("Status 400 - Not an ID ", () => {
+    return request(app).get("/api/articles/no-an-id/comments").expect(400);
+  });
+  test("Status 404 - Non existent ID ", () => {
+    return request(app).get("/api/articles/9999/comments").expect(404);
+  });
+  test("Status 200 - Valid ID, no comments ", () => {
+    return request(app).get("/api/articles/11/comments").expect(200);
   });
 });
-
 test("status: 404 responds with message when path not found", () => {
   return request(app)
     .get("/api/trees")
@@ -254,7 +359,7 @@ test("status: 404 responds with message when path not found", () => {
     });
 });
 
-describe.only("DELETE /api/comments/:comment_id", () => {
+describe("DELETE /api/comments/:comment_id", () => {
   test("status:204 - should return nothing", () => {
     return request(app).delete("/api/comments/1").expect(204);
   });
